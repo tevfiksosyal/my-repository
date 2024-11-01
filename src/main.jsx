0611ef78655da1b976869ec34ea28e1c65de1205
tevfiksosyal <179805268+tevfiksosyal@users.noreply.github.com>
2024-11-01 10:14:24 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/main.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import { BrowserRouter } from 'react-router-dom';
+
+import { Provider } from 'react-redux';
+import './index.css';
+import { myStore } from './store/store.js';
+ReactDOM.createRoot(document.getElementById('root')).render(
+
+  <BrowserRouter>
+    <Provider store={myStore}>
+
+      <App />
+    </Provider>
+  </BrowserRouter>
+);
